9ca3882027d63c2974a41c8097e3f1ed53b15ebb
bug-hou <2510596084@qq.com>
2022-02-22 19:35:25 +0800

    style: 对项目环境进行搭建和配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,9 +10,9 @@ import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+	name: "Home",
+	components: {
+		HelloWorld,
+	},
 });
 </script>
